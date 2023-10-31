6b80ce5dc19c91e0e99c0600dbafa017a3ba095b
Marcel Zama <92083030+MarcelZama@users.noreply.github.com>
2023-10-31 03:01:35 +0000

    Lab 3

---
+++ labThree/Lab-Three-Barrier/Barrier.h
@@ -0,0 +1,42 @@
+#include "Semaphore.h"
+#include <iostream>
+#include <stdio.h>
+
+/*! \class Barrier
+    \brief A Barrier Implementation
+
+   Uses C++11 features such as mutex and condition variables to implement a Barrier class using Semaphores
+
+*/
+class Barrier
+{
+private:
+
+  int count;
+  int threadNum;
+  bool condition;
+  std::shared_ptr<Semaphore> mutex;
+  std::shared_ptr<Semaphore> barrier1;
+  std::shared_ptr<Semaphore> barrier2;
+
+
+public:
+
+  Barrier();
+  ~Barrier();
+  Barrier(int count);
+  void setCount(int count);
+  //int getCount();
+  void waitForAll();
+
+
+
+  //test
+
+  int getCount();
+  int getMutexCount();
+  int getEntryCount();
+  int getExitCount();
+};
+
+
